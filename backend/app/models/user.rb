0b1dd8f323e2d6a2327da6bd7d0c2e187112a161
mmcneeley <mmcneeley@gmail.com>
2018-05-25 13:50:38 -0400

    got everything rotating and my timer working

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+
   has_many :interviews
   has_many :interview_questions, through: :interviews
+
+  has_secure_password
 
 end
---
+++ frontend/src/components/questions.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+class Questions extends React.Component {
+
+  state = {
+    questions: [],
+    current_question: {},
+    timer: 0,
+    interval: null,
+  }
+
+  componentDidMount() {
+
+    // fetch the data to display questions
+    fetch('http://localhost:5000/api/v1/questions')
+      .then(response => response.json())
+      .then(api_questions => {
+        this.setState({
+          questions: api_questions,
+          current_question: {content: "Press enter to start"}
+        })
+      })
+
+    // listen for key events
+    document.addEventListener('keyup', (e) => {
+      if (e.key === " " || e.key === "Enter" || e.key === "ArrowLeft" || e.key === "ArrowRight") {
+        console.log(e)
+        console.log(this.state.current_question.content)
+        this.refreshQuestion()
+      } else {
+        console.log("Not a key I want to track");
+      }
+    })
+  }
+
+  refreshQuestion = () => {
+    if (this.state.questions.length > 0) {
+      this.setState({
+        current_question: this.state.questions[Math.floor(Math.random()*this.state.questions.length)]
+      })
+      this.startTimer()
+    } else {}
+  }
+
+  startTimer = () => {
+    clearInterval(this.state.interval)
+    this.refreshTimer()
+    let timer = setInterval(() => {
+      this.setState({
+        timer: this.state.timer + 1,
+      })
+    }, 1000)
+    this.setState({
+      interval: timer
+    })
+  }
+
+  refreshTimer = () => {
+    this.setState({
+      timer: 0
+    })
+  }
+
+  render(){
+    console.log(this.state);
+    return(
+      <div>
+        <p>{this.state.current_question.content}</p>
+        <p>{this.state.timer}</p>
+      </div>
+    )
+  }
+}
+
+export default Questions;
